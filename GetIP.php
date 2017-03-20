45814e273d866feb029e0dd6749f8c73ab9a4471
yeygt <y.e.ygt@hotmail.com>
2017-03-20 22:15:10 +0300

    getip

---
+++ GetIP.php
@@ -0,0 +1,21 @@
+<?php
+function GetIP(){
+	if(getenv("HTTP_CLIENT_IP")) {
+ 		$ip = getenv("HTTP_CLIENT_IP");
+ 	} elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+ 		$ip = getenv("HTTP_X_FORWARDED_FOR");
+ 		if (strstr($ip, ',')) {
+ 			$tmp = explode (',', $ip);
+ 			$ip = trim($tmp[0]);
+ 		}
+ 	} elseif(getenv("REMOTE_ADDR")) {
+	$ip = getenv("REMOTE_ADDR");	
+	} else {
+ 	$ip = 'localhost';
+ 	}
+	return $ip;
+}
+
+echo GetIP();
+echo '<br />';
+echo $_SERVER['REMOTE_ADDR'];
